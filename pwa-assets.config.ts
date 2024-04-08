4b1f908b951803c8fa23cb4b68c5e6f2c0ad28a8
Stefano Soro <stefano.soro@sap.com>
2024-04-08 16:14:55 -0300

    chore: refactor pwa icons and assets

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -3,6 +3,12 @@ import { defineConfig, minimal2023Preset as preset } from '@vite-pwa/assets-gene
 export default defineConfig({
   preset,
   images: [
-    'public/vite.svg'
+    'public/apple-touch-icon.png',
+    'public/favicon.ico',
+    'public/logo.svg',
+    'public/pwa-64x64.png',
+    'public/pwa-192x192.png',
+    'public/pwa-512x512.png',
+    'public/mask-icon.png',
   ]
 })
--- vite.config.ts
+++ vite.config.ts
@@ -16,28 +16,27 @@ export default defineConfig({
         description: 'Description',
         theme_color: '#ffffff',
         icons: [
-            {
-                src: 'favicon-32x32.png',
-                sizes: '32x32',
-                type: 'image/png'
-            },
-            {
-              src: 'android-chrome-192x192.png',
-              sizes: '192x192',
-              type: 'image/png'
-            },
-            {
-                src: 'android-chrome-512x512.png',
-                sizes: '512x512',
-                type: 'image/png',
-                purpose: 'any'
-            },
-            {
-                src: 'mask-icon.png',
-                sizes: '512x512',
-                type: 'image/png',
-                purpose: 'maskable'
-            }
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+              src: 'mask-icon.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable'
+          }
         ],
       }, 
     })
